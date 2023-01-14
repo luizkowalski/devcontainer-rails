67db84e0648446497117dc3c7643f14908ae5d1f
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-01-13 23:36:46 -0300

    Updated to Ruby 3.2, added libvips to apt-packages list

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+echo "Setting SSH password for vscode user..."
 # Set up vscode user for SSH
 sudo usermod --password $(echo vscode | openssl passwd -1 -stdin) vscode
 
+echo "Updating RubyGems..."
+gem update --system
+
+echo "Installing dependencies..."
 bundle install
 yarn install
 
+echo "Copying database.yml..."
 cp config/database.yml.example config/database.yml
 
-bin/rails db:create db:schema:load db:seed
+echo "Creating database..."
+bin/rails db:create db:schema:load db:migrate db:seed
+
+echo "Done!"
