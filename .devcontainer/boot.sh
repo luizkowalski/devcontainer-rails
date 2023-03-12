48ba3c7e09f8fddd6c00e761cc95bb4ba0b59e09
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-03-12 20:06:56 +0100

    boot.sh changes: * Only copy database.yml if it doesn't already exist * Don't run db:schema:load * Don't install bundler docs

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -4,16 +4,16 @@ echo "Setting SSH password for vscode user..."
 sudo usermod --password $(echo vscode | openssl passwd -1 -stdin) vscode
 
 echo "Updating RubyGems..."
-gem update --system
+gem update --system -N
 
 echo "Installing dependencies..."
 bundle install
 yarn install
 
 echo "Copying database.yml..."
-cp config/database.yml.example config/database.yml
+cp -n config/database.yml.example config/database.yml
 
 echo "Creating database..."
-bin/rails db:create db:schema:load db:migrate db:seed
+bin/rails db:create db:migrate db:seed
 
 echo "Done!"
